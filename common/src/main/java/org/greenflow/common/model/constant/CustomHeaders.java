36a6913d33cc44a732d2915dca180c4d02b83ed0
AntonPiekhotin <pehotin.anton@gmail.com>
2025-04-03 23:47:12 +0300

    refactored custom headers to class

--- common/src/main/java/org/greenflow/common/model/constant/CustomHeaders.java
+++ common/src/main/java/org/greenflow/common/model/constant/CustomHeaders.java
@@ -1,4 +1,15 @@
 package org.greenflow.common.model.constant;
 
-public enum CustomHeaders {
+/**
+ * Custom HTTP headers used in the application.
+ */
+public class CustomHeaders {
+
+    public static final String X_EMAIL = "X-Email";
+    public static final String X_ROLES = "X-Roles";
+    public static final String X_USER_ID = "X-User-Id";
+
+    private CustomHeaders() {
+    }
+
 }
