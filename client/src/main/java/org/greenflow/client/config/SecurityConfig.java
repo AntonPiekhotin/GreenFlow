0825afe423eb237e68415c86953b313557e748d9
AntonPiekhotin <pehotin.anton@gmail.com>
2025-04-07 17:31:16 +0300

    checkstyle

--- client/src/main/java/org/greenflow/client/config/SecurityConfig.java
+++ client/src/main/java/org/greenflow/client/config/SecurityConfig.java
@@ -11,8 +11,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
