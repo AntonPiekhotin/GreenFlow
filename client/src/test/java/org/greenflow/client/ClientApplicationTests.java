809229d74382b89f6eb9611bf7528cccf61128d2
AntonPiekhotin <pehotin.anton@gmail.com>
2025-05-04 22:39:21 +0300

    ci cd for client

--- client/src/test/java/org/greenflow/client/ClientApplicationTests.java
+++ client/src/test/java/org/greenflow/client/ClientApplicationTests.java
@@ -1,9 +1,7 @@
 package org.greenflow.client;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ClientApplicationTests {
 
     @Test
