87b0e98f0c59789f9c74c5e53d38ecdd55dd7032
AntonPiekhotin <pehotin.anton@gmail.com>
2025-05-04 22:43:54 +0300

    ci cd for equipment

--- equipment/src/test/java/org/greenflow/equipment/EquipmentApplicationTests.java
+++ equipment/src/test/java/org/greenflow/equipment/EquipmentApplicationTests.java
@@ -1,9 +1,7 @@
 package org.greenflow.equipment;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class EquipmentApplicationTests {
 
 	@Test
