067d138f959950b202006a5cc376875f8172df47
AntonPiekhotin <pehotin.anton@gmail.com>
2025-05-04 22:59:48 +0300

    ci cd for open-order

--- open-order/src/test/java/org/greenflow/openorder/OpenOrderApplicationTests.java
+++ open-order/src/test/java/org/greenflow/openorder/OpenOrderApplicationTests.java
@@ -1,9 +1,7 @@
 package org.greenflow.openorder;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class OpenOrderApplicationTests {
 
 	@Test
