b72d54b9ff0319929ee6e2e9b275733ce1ffa072
AntonPiekhotin <pehotin.anton@gmail.com>
2025-05-04 22:55:44 +0300

    ci cd for notification

--- notification/src/test/java/org/greenflow/notification/NotificationApplicationTests.java
+++ notification/src/test/java/org/greenflow/notification/NotificationApplicationTests.java
@@ -1,9 +1,7 @@
 package org.greenflow.notification;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class NotificationApplicationTests {
 
     @Test
