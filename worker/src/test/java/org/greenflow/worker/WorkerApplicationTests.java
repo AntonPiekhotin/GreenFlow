c504176d4c2ee259ff5e953f43eca2ff1b4e0682
AntonPiekhotin <pehotin.anton@gmail.com>
2025-05-04 23:19:12 +0300

    ci cd for worker

--- worker/src/test/java/org/greenflow/worker/WorkerApplicationTests.java
+++ worker/src/test/java/org/greenflow/worker/WorkerApplicationTests.java
@@ -1,9 +1,7 @@
 package org.greenflow.worker;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class WorkerApplicationTests {
 
     @Test
