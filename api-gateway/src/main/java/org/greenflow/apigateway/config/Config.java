5b779ac0c4f12b7046058757e7c81c3fd37d0732
AntonPiekhotin <pehotin.anton@gmail.com>
2025-05-13 22:27:25 +0300

    add service configurations for various microservices in application.yml and update server ports

--- api-gateway/src/main/java/org/greenflow/apigateway/config/Config.java
+++ api-gateway/src/main/java/org/greenflow/apigateway/config/Config.java
@@ -2,6 +2,7 @@ package org.greenflow.apigateway.config;
 
 import lombok.RequiredArgsConstructor;
 import org.greenflow.apigateway.filter.AuthenticationFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,13 +14,44 @@ public class Config {
 
     private final AuthenticationFilter filter;
 
+    @Value("${service.auth.host}")
+    private static String AUTH_SERVICE_HOST;
+    @Value("${service.auth.port}")
+    private static String AUTH_SERVICE_PORT;
+
+    @Value("${service.client.host}")
+    private static String CLIENT_SERVICE_HOST;
+    @Value("${service.client.port}")
+    private static String CLIENT_SERVICE_PORT;
+
+    @Value("${service.worker.host}")
+    private static String WORKER_SERVICE_HOST;
+    @Value("${service.worker.port}")
+    private static String WORKER_SERVICE_PORT;
+
+    @Value("${service.garden.host}")
+    private static String GARDEN_SERVICE_HOST;
+    @Value("${service.garden.port}")
+    private static String GARDEN_SERVICE_PORT;
+
+    @Value("${service.order.host}")
+    private static String ORDER_SERVICE_HOST;
+    @Value("${service.order.port}")
+    private static String ORDER_SERVICE_PORT;
+
+    @Value("${service.open-order.host}")
+    private static String OPEN_ORDER_SERVICE_HOST;
+    @Value("${service.open-order.port}")
+    private static String OPEN_ORDER_SERVICE_PORT;
+
+
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("auth-service", r -> r.path("/api/v1/auth/register")
-                        .uri("lb://auth-service"))
+                        .uri("http://" + AUTH_SERVICE_HOST + ":" + AUTH_SERVICE_PORT))
                 .route("auth-service", r -> r.path("/api/v1/auth/login")
-                        .uri("lb://auth-service"))
+                        .uri("http://" + AUTH_SERVICE_HOST + ":" + AUTH_SERVICE_PORT))
                 .route("client", r -> r.path("/api/v1/client/**")
                         .filters(f -> f.filter(filter))
                         .uri("lb://client"))
