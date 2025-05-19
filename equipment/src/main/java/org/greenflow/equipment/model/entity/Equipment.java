cbc191990ba492aad0c5f88238272ae5d34c41e0
AntonPiekhotin <pehotin.anton@gmail.com>
2025-05-19 19:33:23 +0300

    created leasing module

--- equipment/src/main/java/org/greenflow/equipment/model/entity/Equipment.java
+++ equipment/src/main/java/org/greenflow/equipment/model/entity/Equipment.java
@@ -3,6 +3,8 @@ package org.greenflow.equipment.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,12 +13,12 @@ import lombok.NonNull;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
-import org.greenflow.equipment.model.entity.types.Fertilizer;
 import org.greenflow.equipment.model.entity.types.Lawnmower;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.math.BigDecimal;
 
 @Document(collection = "equipment")
 @Getter
@@ -27,7 +29,6 @@ import org.springframework.data.mongodb.core.mapping.DocumentReference;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = Fertilizer.class, name = "fertilizer"),
         @JsonSubTypes.Type(value = Lawnmower.class, name = "lawnmower")}
 )
 public abstract class Equipment {
@@ -35,9 +36,16 @@ public abstract class Equipment {
     @Id
     String id;
 
-    @DocumentReference(lazy = false)
+    String leasedBy;
+
+    @NotNull
+    BigDecimal dailyLeasingPrice;
+
+    @NotNull
+    @DocumentReference
     Warehouse warehouse;
 
+    @NotBlank
     String name;
 
     String description;
